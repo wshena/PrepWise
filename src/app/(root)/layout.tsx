ec467a0308a4bb449213bbeaeaeffb422b30cfe9
poppywydy <adeliasekar7@gmail.com>
2025-03-22 16:32:20 +0700

    adding navbar to rootlayout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,8 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const RootLayout = ({children}:{children:React.ReactNode}) => {
   return (
-    <div>{children}</div>
+    <div className='root-layout'>
+      <nav className="">
+        <Link href={'/'} className='flex items-center gap-2'>
+          <Image src={'/logo.svg'} alt='logo' width={38} height={32} />
+          <h2 className="text-primary-100">PrepWise</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
   )
 }
 
