c8b2526e7f2bff08231c292e5c8ca04bf7d4e323
poppywydy <adeliasekar7@gmail.com>
2025-03-22 14:29:37 +0700

    create FormField component, for reusable input field on form for both sign-in and sign-up

---
+++ src/components/FormField.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { FormControl, FormDescription, FormItem, FormLabel, FormMessage } from './ui/form'
+import { Input } from './ui/input'
+import { Controller, FieldValues, Path, Control } from 'react-hook-form'
+
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>,
+  name: Path<T>,
+  label: string,
+  placeholder?: string,
+  type?: 'text' | 'email' | 'password' | 'file'
+}
+
+const FormField = <T extends FieldValues>({control, name, label, placeholder, type='text'}:FormFieldProps<T>) => {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({field}) => (
+        <FormItem>
+          <FormLabel className='label'>{label}</FormLabel>
+          <FormControl>
+            <Input className='input' type={type} placeholder={placeholder} {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />  
+  )
+}
+
+export default FormField
